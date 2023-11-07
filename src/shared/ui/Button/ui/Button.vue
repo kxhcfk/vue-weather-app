c123846b555f8f5964252335d31cef495cab4bf1
kxhcfk <reveersed@mail.ru>
2023-11-07 21:11:17 +0300

    feat(ui): button

---
+++ src/shared/ui/Button/ui/Button.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+interface Props {}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+    <button
+        :class="{
+            button: true
+        }"
+    >
+        <slot></slot>
+    </button>
+</template>
+
+<style scoped>
+.button {
+    padding: 0;
+    margin: 0;
+    display: inline-flex;
+    background: none;
+    border: 0;
+}
+</style>
