9a2d92594c0e7284b6aa9d599040d84a0cefdcd0
kxhcfk <reveersed@mail.ru>
2023-11-07 21:10:46 +0300

    feat(widgets): simple header markup

---
+++ src/widgets/Header/ui/Header.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import SearchIcon from '@/shared/assets/icons/search.svg'
+import { Button } from "@/shared/ui/Button";
+</script>
+
+<template>
+    <header class="header">
+        <div class="container">
+            <div class="header__wrapper">
+                <Button>
+                    <SearchIcon/>
+                </Button>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style>
+.header {
+    position: sticky;
+    top: 0;
+    backdrop-filter: blur(5px);
+}
+
+.header__wrapper {
+    padding-block: var(--space-s);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+</style>
