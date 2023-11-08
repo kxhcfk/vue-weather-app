7f464dc97040001ae248444a10cef7455d0d1144
kxhcfk <reveersed@mail.ru>
2023-11-08 15:18:11 +0300

    refactor: update weather types

--- src/shared/types/weather.ts
+++ src/shared/types/weather.ts
@@ -1,47 +1,54 @@
 export interface Weather {
-    location: WeatherLocation;
-    current: WeatherCurrent;
-}
-
-export interface WeatherCurrent {
-    last_updated_epoch: number;
-    last_updated: string;
-    temp_c: number;
-    temp_f: number;
-    is_day: number;
-    condition: WeatherCondition;
-    wind_mph: number;
-    wind_kph: number;
-    wind_degree: number;
-    wind_dir: string;
-    pressure_mb: number;
-    pressure_in: number;
-    precip_mm: number;
-    precip_in: number;
-    humidity: number;
-    cloud: number;
-    feelslike_c: number;
-    feelslike_f: number;
-    vis_km: number;
-    vis_miles: number;
-    uv: number;
-    gust_mph: number;
-    gust_kph: number;
-}
-
-export interface WeatherCondition {
-    text: string;
-    icon: string;
-    code: number;
-}
-
-export interface WeatherLocation {
+    coord: WeatherCoord;
+    weather: WeatherElement[];
+    base: string;
+    main: WeatherMain;
+    visibility: number;
+    wind: WeatherWind;
+    clouds: WeatherClouds;
+    dt: number;
+    sys: WeatherSys;
+    timezone: number;
+    id: number;
     name: string;
-    region: string;
-    country: string;
-    lat: number;
+    cod: number;
+}
+
+export interface WeatherClouds {
+    all: number;
+}
+
+export interface WeatherCoord {
     lon: number;
-    tz_id: string;
-    localtime_epoch: number;
-    localtime: string;
+    lat: number;
+}
+
+export interface WeatherMain {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+    sea_level: number;
+    grnd_level: number;
+}
+
+export interface WeatherSys {
+    country: string;
+    sunrise: number;
+    sunset: number;
+}
+
+export interface WeatherElement {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+}
+
+export interface WeatherWind {
+    speed: number;
+    deg: number;
+    gust: number;
 }
